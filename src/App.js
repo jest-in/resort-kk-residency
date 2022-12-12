3dbbe68afa13b0d8ce56e4b1af6d0ede9a08e53a
jest-in <jestin9900767795@gmail.com>
2022-12-12 15:50:44 +0530

    Added new pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+// Components import
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import AllRooms from "./pages/allrooms";
+import OneRoom from "./pages/oneroom";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<SignIn/>}/>
+        <Route path="register" element={<SignUp/>}/>
+        <Route path="all-rooms" element={<AllRooms/>}/>
+        <Route path="room" element={<OneRoom/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
