73fdb11e1bd4dc68ce295f68a83f6f25c6cb11ab
jest-in <jestin9900767795@gmail.com>
2022-12-12 18:48:53 +0530

    Components for svg created

---
+++ src/Assets/Icons/Instagram.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Instagram() {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16 1H6C3.23858 1 1 3.23858 1 6V16C1 18.7614 3.23858 21 6 21H16C18.7614 21 21 18.7614 21 16V6C21 3.23858 18.7614 1 16 1Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.0001 10.3698C15.1235 11.2021 14.9813 12.052 14.5938 12.7988C14.2063 13.5456 13.5932 14.1512 12.8417 14.5295C12.0902 14.9077 11.2385 15.0394 10.4079 14.9057C9.5772 14.7721 8.80983 14.3799 8.21491 13.785C7.61999 13.1901 7.22781 12.4227 7.09414 11.592C6.96048 10.7614 7.09214 9.90971 7.4704 9.15819C7.84866 8.40667 8.45426 7.79355 9.20106 7.40605C9.94786 7.01856 10.7978 6.8764 11.6301 6.99981C12.479 7.1257 13.2649 7.52128 13.8718 8.12812C14.4786 8.73496 14.8742 9.52089 15.0001 10.3698Z"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.5 5.5H16.5106"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/Assets/Icons/RatesAndInventory.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function RatesAndInventory() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.58 8.32H17.26M6.73999 14.11H17.27M6.99999 22H17M7.25999 2H16.73C17.38 2 17.96 2.02 18.48 2.09C21.25 2.4 22 3.7 22 7.26V13.58C22 17.14 21.25 18.44 18.48 18.75C17.96 18.82 17.39 18.84 16.73 18.84H7.25999C6.60999 18.84 6.02999 18.82 5.50999 18.75C2.73999 18.44 1.98999 17.14 1.98999 13.58V7.26C1.98999 3.7 2.73999 2.4 5.50999 2.09C6.02999 2.02 6.60999 2 7.25999 2V2Z"
+        stroke="#222222"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.19507 8.30005H7.20407M10.4951 8.30005H10.5041"
+        stroke="#222222"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
