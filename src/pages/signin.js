a00a1fd1881048929d7bdbe245873028c14cd3fa
jest-in <jestin9900767795@gmail.com>
2023-01-19 22:31:34 +0530

    error dialog added for signin and signup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,100 +3,92 @@ import React from 'react';
 // importing images
 import Logo from '../Assets/Logo/KKGroupLogoWhite';
 import Footer from '../components/footer';
+import image from "../Assets/Images/Login Image.jpg";
+import { Link, NavLink } from 'react-router-dom';
 
 export default function SignIn() {
     return (
-        <div className="container">
-            <div className="sign-in-container">
-                <div className="login-div1">
-                    <div className="login-div1-container">
-                        <header>
-                            <nav className="logo-container">
-                                <a href="/">
-                                    <figure>
-                                        <Logo />
-                                    </figure>
-                                </a>
-                            </nav>
-                        </header>
-                        <aside>
-                            <div className="login-head-container">
-                                <h3>Start your journey with us.</h3>
-                            </div>
-                            <figure className="login-image-container">
-                                {/* place the image here */}
-                            </figure>
-                            <div className="login-caption-div">
-                                <p>
-                                    Best rooms in Madikeri at affordable price.{' '}
-                                    <br />
-                                    Contact KK Resort.
-                                </p>
-                            </div>
-                        </aside>
-                    </div>
+      <div className="container">
+        <div className="sign-in-container">
+          <div className="login-div1">
+            <div className="login-div1-container">
+              <header>
+                <nav className="logo-container">
+                  <Link to="/">
+                    <figure>
+                      <Logo />
+                    </figure>
+                  </Link>
+                </nav>
+              </header>
+              <aside>
+                <div className="login-head-container">
+                  <h3>Start your journey with us.</h3>
                 </div>
-                <main className="login-div2">
-                    <div class="login-form-container">
-                        <form action="#">
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="email"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="password"
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <div className="login-create-btn-div">
-                                <button type="submit">Sign in</button>
-                            </div>
-                        </form>
-                        <section className="signin-actions-section">
-                            <a
-                                href="/forgot-password"
-                                className="existing-user-link"
-                            >
-                                Forgot password ?
-                            </a>
-                            <a
-                                href="/register"
-                                className="existing-user-link new-user-link"
-                            >
-                                Dont have an account ?
-                            </a>
-                        </section>
-                    </div>
-                </main>
+                <figure className="login-image-container">
+                  <img src={image} alt="resort" />
+                </figure>
+                <div className="login-caption-div">
+                  <p>
+                    Best rooms in Madikeri at affordable price. <br />
+                    Contact KK Resort.
+                  </p>
+                </div>
+              </aside>
             </div>
-            <Footer />
+          </div>
+          <main className="login-div2">
+            <div className="login-form-container">
+              <form action="#">
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <div className="login-create-btn-div">
+                  <button type="submit">Sign in</button>
+                </div>
+              </form>
+              <nav className="signin-actions-section">
+                <Link to="/forgot-password" className="existing-user-link">
+                  Forgot password ?
+                </Link>
+                <Link
+                  to="/register"
+                  className="existing-user-link new-user-link"
+                >
+                  Dont have an account ?
+                </Link>
+              </nav>
+            </div>
+          </main>
         </div>
+        <Footer />
+      </div>
     );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,131 +1,116 @@
 import React from 'react';
 import Footer from '../components/footer';
 import Logo from '../Assets/Logo/KKGroupLogoWhite';
-import Header from '../components/header';
+import image from "../Assets/Images/Login Image.jpg";
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
     return (
-        <div className="container">
-            <div className="sign-in-container">
-                <div className="login-div1">
-                    <div className="login-div1-container">
-                        <header>
-                            <nav className="logo-container">
-                                <a href="/">
-                                    <figure>
-                                        <Logo />
-                                    </figure>
-                                </a>
-                            </nav>
-                        </header>
-                        <aside>
-                            <div className="login-head-container">
-                                <h3>Start your journey with us.</h3>
-                            </div>
-                            <figure className="login-image-container">
-                                {/* place the image here */}
-                            </figure>
-                            <div className="login-caption-div">
-                                <p>
-                                    Best rooms in Madikeri at affordable price.{' '}
-                                    <br />
-                                    Contact KK Resort.
-                                </p>
-                            </div>
-                        </aside>
-                    </div>
+      <div className="container">
+        <div className="sign-in-container">
+          <div className="login-div1">
+            <div className="login-div1-container">
+              <header>
+                <nav className="logo-container">
+                  <Link to="/">
+                    <figure>
+                      <Logo />
+                    </figure>
+                  </Link>
+                </nav>
+              </header>
+              <aside>
+                <div className="login-head-container">
+                  <h3>Start your journey with us.</h3>
                 </div>
-                <main className="login-div2">
-                    <div class="login-form-container">
-                        <form action="#">
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="name"
-                                >
-                                    Name
-                                </label>
-                                <input
-                                    id="name"
-                                    name="name"
-                                    type="text"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="email"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="phone"
-                                >
-                                    Phone number
-                                </label>
-                                <input
-                                    id="phone"
-                                    name="phone"
-                                    type="tel"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <p className="login-entries-div">
-                                <label
-                                    class="login-entries-label"
-                                    htmlFor="password"
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    placeholder=""
-                                    required
-                                />
-                                <label htmlFor="error" class="login-error">
-                                    Error Message
-                                </label>
-                            </p>
-                            <div className="login-create-btn-div">
-                                <button type="submit">Create account</button>
-                            </div>
-                        </form>
-                        <section className="signin-actions-section">
-                            <a
-                                href="/forgot-password"
-                                className="existing-user-link"
-                            >
-                                Already have an account ?
-                            </a>
-                        </section>
-                    </div>
-                </main>
+                <figure className="login-image-container">
+                  <img src={image} alt="resort" />
+                </figure>
+                <div className="login-caption-div">
+                  <p>
+                    Best rooms in Madikeri at affordable price. <br />
+                    Contact KK Resort.
+                  </p>
+                </div>
+              </aside>
             </div>
-            <Footer />
+          </div>
+          <main className="login-div2">
+            <div className="login-form-container">
+              <form action="#">
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="phone">
+                    Phone number
+                  </label>
+                  <input
+                    id="phone"
+                    name="phone"
+                    type="tel"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <p className="login-entries-div">
+                  <label className="login-entries-label" htmlFor="password">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder=""
+                    required
+                  />
+                  <dialog open>
+                    <p className="login-error">Errror Message</p>
+                  </dialog>
+                </p>
+                <div className="login-create-btn-div">
+                  <button type="submit">Create account</button>
+                </div>
+              </form>
+              <nav className="signin-actions-section">
+                <Link to="/login" className="existing-user-link">
+                  Already have an account ?
+                </Link>
+              </nav>
+            </div>
+          </main>
         </div>
+        <Footer />
+      </div>
     );
 }
