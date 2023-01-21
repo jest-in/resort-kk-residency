5f5f186e38954eee7d0786961ee3a4284993eb9a
shravyass2003@gmail.com <shravyass2003@gmail.com>
2023-01-21 21:01:06 +0530

    footer modified

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,61 +6,43 @@ import Youtube from '../Assets/Icons/Youtube';
 export default function Footer() {
     return (
         <footer className="login-footer">
-            <div>
-                <p>
-                    © <time dateTime={2022}>2022</time>
-                </p>
-                <address>
-                    KK Group of Residency, Virajpet | Designed by Aashan
-                    Creative Studio
-                </address>
-            </div>
-            <ul>
-                <li>
-                    <a href="#">
-                        facebook
-                        <span aria-hidden="true">
-                            {/* place the facebook svg */}
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        instagram
-                        <span aria-hidden="true">
-                            {/* place the instagram svg */}
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        youtube
-                        <span aria-hidden="true">
-                            {/* place the youtube svg */}
-                        </span>
-                    </a>
-                </li>
-            </ul>
-
             <hr className="login-footer-border" />
             <div class="login-footer-container">
                 <div class="login-footer-sec1">
                     <p>
-                        © 2022 KK Groop of Residency, Virajpet | Designed by
-                        Aashan Creative Studio
+                        © <time dateTime={2023}>2023</time>
                     </p>
+                    <address>
+                        KK Group of Residency, Virajpet | Designed by Aashan
+                        Creative Studio
+                    </address>
                 </div>
-                <div class="login-footer-sec2">
-                    <a href="#">
-                        <Facebook />
-                    </a>
-                    <a href="#">
-                        <Instagram />
-                    </a>
-                    <a href="#">
-                        <Youtube />
-                    </a>
-                </div>
+                <ul class="login-footer-sec2">
+                    <li>
+                        <a href="#">
+                            facebook
+                            <span aria-hidden="true">
+                                <Facebook />
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            instagram
+                            <span aria-hidden="true">
+                                <Instagram />
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            youtube
+                            <span aria-hidden="true">
+                                <Youtube />
+                            </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </footer>
     );
