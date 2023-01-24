e52ba969b507c0a51ea57435ef9d4e0aa84d9403
shravyass2003@gmail.com <shravyass2003@gmail.com>
2023-01-24 18:11:09 +0530

    book-room page created

--- src/pages/User/book-room.js
+++ src/pages/User/book-room.js
@@ -1,65 +1,132 @@
-import React from 'react'
-import Footer from '../../components/footer'
-import Header from '../../components/header'
-import DatePicker from "react-datepicker";
-import { addDays } from "date-fns";
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
+import DatePicker from 'react-datepicker';
+import { addDays } from 'date-fns';
+import 'react-datepicker/dist/react-datepicker.css';
+import cat1 from '../../Assets/Images/Room Category 1.jpg';
 
 export default function BookRoom() {
-  return (
-    <>
-      <Header />
-      <main>
-        <section>
-          <h3>Select date</h3>
-          <p>Dates in green color are the available dates</p>
-          <DatePicker
-            inline
-            disabledKeyboardNavigation
-            readOnly
-            minDate={new Date()}
-            highlightDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
-          />
-          <form>
-            <label htmlFor="from">From</label>
-            <input type="text" id="from" name="from" />
-            <label htmlFor="to">To</label>
-            <input type="text" id="to" name="to" />
-            <p>
-              <span>Note:</span>
-              <span>
-                We’ll call or text you to confirm your number. Standard message
-                and data rates apply
-              </span>
-            </p>
-            <button type="submit">Book now</button>
-          </form>
-        </section>
-        <div>
-          <figure>
-            {/* image */}
-            <figcaption>
-              <span>Luxury Plus</span>
-              <span>★ 4.9 (254)</span>
-            </figcaption>
-          </figure>
-          <hr />
-          <p>
-            <span>From</span>
-            <time dateTime="2022-10-22">22-Oct-2022</time>
-          </p>
-          <p>
-            <span>To</span>
-            <time dateTime="2022-10-24">24-Oct-2022</time>
-          </p>
-          <hr />
-          <p>
-            <span>Total</span>
-            <span>Rs.1500</span>
-          </p>
+    return (
+        <div className="container">
+            <Header />
+            <main className="bookroom-container">
+                <section className="bookroom-entries-container">
+                    <div className="singleroom-calender-details">
+                        <h2>Select date</h2>
+                        <p className="bookroom-selectdate-note">
+                            Dates in green color are the available dates
+                        </p>
+                    </div>
+                    <form>
+                        <DatePicker
+                            inline
+                            disabledKeyboardNavigation
+                            readOnly
+                            minDate={new Date()}
+                            highlightDates={[
+                                addDays(new Date(), 1),
+                                addDays(new Date(), 5)
+                            ]}
+                        />
+                        <div className="bookroom-dateselect-container">
+                            <div className="bookroom-fromcontainer">
+                                <label
+                                    className="bookroom-form-label"
+                                    htmlFor="from"
+                                >
+                                    From
+                                </label>
+                                <input
+                                    className="bookroom-form-input"
+                                    type="text"
+                                    id="from"
+                                    name="from"
+                                />
+                            </div>
+                            <div className="bookroom-tocontainer">
+                                <label
+                                    className="bookroom-form-label"
+                                    htmlFor="to"
+                                >
+                                    To
+                                </label>
+                                <input
+                                    className="bookroom-form-input"
+                                    type="text"
+                                    id="to"
+                                    name="to"
+                                />
+                            </div>
+                        </div>
+                        <p className="bookroom-note-div">
+                            <span className="bookroom-note-label">Note:</span>
+                            <span className="bookroom-note-desc">
+                                We’ll call or text you to confirm your number.
+                                Standard message and data rates apply
+                            </span>
+                        </p>
+                        <button
+                            className="book-now-btn bookroom-booknow-btn"
+                            type="submit"
+                        >
+                            Book now
+                        </button>
+                    </form>
+                </section>
+                <div className="bookroom-checkout-container">
+                    <div className="bookroom-checkout-borderdiv">
+                        <figure className="bookroom-checkout-img-div">
+                            <img
+                                className="bookroom-img-cat"
+                                src={cat1}
+                                alt="room category"
+                            />
+                            <figcaption className="room-rating bookroom-rating">
+                              <h2>Luxuary Plus</h2>
+                                <p className="room-rating">
+                                    <span className="rating-star">★</span>{' '}
+                                    <span className="rating-point">4.9</span>{' '}
+                                    <span className="rating-user-count">
+                                        (254)
+                                    </span>
+                                </p>
+                            </figcaption>
+                        </figure>
+                        <hr className="whatitoffers-hr" />
+                        <p className="bookroom-checkout-time-from">
+                            <span className="bookroom-form-label booknow-checkout-from-label">
+                                From
+                            </span>
+                            <time
+                                className="bookroom-checkout-time"
+                                dateTime="2022-10-22"
+                            >
+                                22-Oct-2022
+                            </time>
+                        </p>
+                        <p className="bookroom-checkout-time-from">
+                            <span className="bookroom-form-label booknow-checkout-from-label">
+                                To
+                            </span>
+                            <time
+                                className="bookroom-checkout-time"
+                                dateTime="2022-10-24"
+                            >
+                                24-Oct-2022
+                            </time>
+                        </p>
+                        <hr className="whatitoffers-hr bookroom-checkout-hr" />
+                        <p className="bookroom-checkout-amt-div">
+                            <span>Total</span>
+                            <span className="bookroom-checkout-price">
+                                Rs.1500
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
-      </main>
-      <Footer />
-    </>
-  );
+    );
 }
